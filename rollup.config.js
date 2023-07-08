6bb0c8419934dd2646bb092cec55f09528930d87
alex bu <4lex.bu@gmail.com>
2023-07-08 18:53:56 +0400

    Automatic bundling to /dist/compressed folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import zip from 'rollup-plugin-zip';
+
 import {
   chromeExtension,
   simpleReloader,
 } from 'rollup-plugin-chrome-extension';
+
+import manifest from './src/manifest.json' assert { type: 'json' };
 
 export default {
   input: 'src/manifest.json',
@@ -18,5 +22,8 @@ export default {
     resolve(),
     commonjs(),
     typescript(),
+    zip({
+      file: `compressed/${manifest.name}-${manifest.version}.zip`,
+    }),
   ],
 };
